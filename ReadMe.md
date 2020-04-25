4c5ddda33d39e138e310545eb14fa2c2a085d85d
dli <davidli012345@gmail.com>
2020-04-24 18:43:28 -0700

    adding new packages in

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+# Stock Notifications
+
+System to notify me when certain big news hit from the google news api via using the newsapi.
+
+https://docs.microsoft.com/en-us/dotnet/api/system.environment.getenvironmentvariable?view=netframework-4.8
+
+
+Although some of the code is a little inefficient, the program scope is pretty simple, I used bonsai for the free elasticsearch hosting (0.1GB), highly doubt my script will pass 100 MB anytime soon.
---
+++ TestNotifications.cs
@@ -0,0 +1,86 @@
+using Xunit;
+using Xunit.Abstractions;
+using System.Xml;
+using System.Net.Http;
+using System;
+using Elasticsearch.Net;
+using System.IO;
+namespace news_test
+{
+    public class Guid
+    {
+        public string guid { get; set; }
+    }
+    public class TestNotifications
+    {
+        private readonly ITestOutputHelper output;
+        public TestNotifications(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        // [Fact]
+        // public void FailingTest()
+        // {
+        //     Assert.Equal(5, Add(2, 2));
+        // }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        [Fact]
+        public void FailXmlTest() {
+            // Create the XmlDocument.
+            XmlDocument doc = new XmlDocument();
+            Assert.Throws<XmlException>(() => doc.LoadXml("<item><nam>wrench</name></item>"));
+            // Assert.Throws(doc.LoadXml("<item><nam>wrench</name></item>"));
+            output.WriteLine("This is output from");
+        }
+        [Fact]
+        public void FetchDataTest()
+        {
+            Program p1 = new Program();  //a object of class  
+            string data = Program.FetchData("nextech ar");
+            Assert.True(data.Length > 100, "The data was not greater than 100 characters");
+            // make sure the data is valid xml
+        }
+        [Fact]
+        public void DiscordMessageTest()
+        {
+            Program p1 = new Program();  //a object of class  
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml("<book>" +
+            "  <title>Oberon's Legacy</title>" +
+            "  <price>5.95</price>" +
+            "</book>");
+            XmlNode newElem = doc.CreateNode("element", "pages", "");
+            Program.SendItemToDiscord(newElem);
+            Assert.True(true, "The data was not greater than 100 characters");
+            // make sure the data is valid xml
+        }
+        [Fact]
+        public async void AddDocToEsTest()
+        {
+          string esInstance = Environment.GetEnvironmentVariable("ES_INSTANCE");
+          var settings = new ConnectionConfiguration(new Uri(esInstance))
+            .RequestTimeout(TimeSpan.FromMinutes(2));
+
+          var lowlevelClient = new ElasticLowLevelClient(settings);
+          var sample_guid = new Guid
+          {
+              guid = "Martijn"
+          };
+          var asyncIndexResponse = await lowlevelClient.IndexAsync<StringResponse>("test_post", "1", PostData.Serializable(sample_guid)); 
+          string responseString = asyncIndexResponse.Body;
+          Console.WriteLine(responseString);
+        }
+    }
+}
